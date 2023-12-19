b6b5e60097c3106699bbfa42482f016471f4b7c5
DeshnaBera <beradeshna@gmail.com>
2023-12-19 11:45:08 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,139 @@
+provider "aws" {
+ region = "us-east-1"  # Choose your preferred region
+}
+
+resource "aws_dynamodb_table" "students" {
+ name           = "students"
+ billing_mode   = "PAY_PER_REQUEST"
+ hash_key       = "studentId"
+ attribute {
+   name = "studentId"
+   type = "S"
+ }
+ provisioned_throughput{
+    read_capacity = 1
+    write_capacity = 1
+ }
+}
+
+resource "aws_lambda_function" "add_student" {
+ function_name = "add_student"
+ handler      = "lambda.add_student"
+ runtime      = "python3.8"
+ memory_size = 128
+ role = aws_iam_role.lambda.arn
+ source_code_hash = filebase64("${path.module}/lambda/add_student.zip")
+ environment {
+   variables = {
+     DYNAMODB_TABLE = aws_dynamodb_table.students.name
+   }
+ }
+}
+
+resource "aws_lambda_function" "list_students" {
+ function_name = "list_students"
+ handler      = "lambda.list_students"
+ runtime      = "python3.8"
+ role = aws_iam_role.lambda.arn
+ source_code_hash = filebase64("${path.module}/lambda/list_students.zip")
+ environment {
+   variables = {
+     DYNAMODB_TABLE = aws_dynamodb_table.students.name
+   }
+ }
+}
+
+resource "aws_iam_role" "lambda" {
+ name = "lambda_execution_role"
+ assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_api_gateway_rest_api" "students_api" {
+ name        = "students_api"
+ description = "API for managing students"
+}
+
+resource "aws_api_gateway_resource" "add_student_resource" {
+ rest_api_id = aws_api_gateway_rest_api.students_api.id
+ parent_id   = aws_api_gateway_rest_api.students_api.root_resource_id
+ path_part   = "addStudent"
+}
+
+resource "aws_api_gateway_resource" "list_students_resource" {
+ rest_api_id = aws_api_gateway_rest_api.students_api.id
+ parent_id   = aws_api_gateway_rest_api.students_api.root_resource_id
+ path_part   = "listStudent"
+}
+
+resource "aws_api_gateway_method" "add_student_method" {
+ rest_api_id   = aws_api_gateway_rest_api.students_api.id
+ resource_id   = aws_api_gateway_resource.add_student_resource.id
+ http_method   = "POST"
+ authorization = "NONE"
+}
+
+resorce "aws_api_gateway_method" "list_students_method" {
+ rest_api_id   = aws_api_gateway_rest_api.students_api.id
+ resource_id   = aws_api_gateway_resource.list_students_resource.id
+ http_method   = "GET"
+ authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "add_student_integration" {
+ rest_api_id             = aws_api_gateway_rest_api.students_api.id
+ resource_id             = aws_api_gateway_resource.add_student_resource.id
+ http_method             = aws_api_gateway_method.add_student_method.http_method
+ integration_http_method = "POST"
+ type                    = "AWS_PROXY"
+ uri                     = aws_lambda_function.add_student.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "list_students_integration" {
+ rest_api_id             = aws_api_gateway_rest_api.students_api.id
+ resource_id             = aws_api_gateway_resource.list_students_resource.id
+ http_method             = aws_api_gateway_method.list_students_method.http_method
+ integration_http_method = "GET"
+ type                    = "AWS_PROXY"
+ uri                     = aws_lambda_function.list_students.invoke_arn
+}
+
+resource "aws_lambda_permission" "add_student_permission" {
+ statement_id  = "AddStudentPermission"
+ action        = "lambda:InvokeFunction"
+ function_name = aws_lambda_function.add_student.function_name
+ principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_lambda_permission" "list_students_permission" {
+ statement_id  = "ListStudentsPermission"
+ action        = "lambda:InvokeFunction"
+ function_name = aws_lambda_function.list_students.function_name
+ principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_deployment" "students_api_deployment" {
+ depends_on = [
+   aws_api_gateway_integration.add_student_integration,
+   aws_api_gateway_integration.list_students_integration
+ ]
+ rest_api_id = aws_api_gateway_rest_api.students_api.id
+ stage_name  = "prod"
+}
+
+output "api_gateway_url" {
+ value = aws_api_gateway_deployment.students_api_deployment.invoke_url
+}
