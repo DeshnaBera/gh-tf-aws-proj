f6ef6b75af7b8c84b5df734cc957860b19fe7c62
DeshnaBera <beradeshna@gmail.com>
2024-01-04 12:23:46 +0530

    broke code

--- main.tf
+++ main.tf
@@ -17,8 +17,6 @@ terraform {
 resource "aws_dynamodb_table" "students" {
  name           = "students"
  billing_mode   = "PROVISIONED"
- read_capacity  = 1
- write_capacity = 1
  hash_key       = "Id"
  attribute {
    name = "Id"
